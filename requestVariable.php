c0b77170f045d4faa0990d61a5684d1323cf7152
mokad@Mokaddes_Ali <mokaddesali20040@gmail.com>
2024-07-10 11:05:00 +0600

    Fourth Commit

---
+++ requestVariable.php
@@ -0,0 +1,27 @@
+<?php
+
+if($_REQUEST["name"] || $_REQUEST["age"])
+{
+    echo "Welcome" . $_POST["name"] . "<br />";
+    echo "You are" . $_POST["age"]. "Years Old";
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="<?php $_PHP_SELF; ?>" method="POST">
+        Name: <input name="name" type="text" />
+        Age: <input name="age" type="text" />
+        <input type ="submit" />
+    </form>
+    
+</body>
+</html>
